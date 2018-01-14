edac94138e647ccac90d43df9126c7db73fb2a95
George Tzinos <geotzinos@gmail.com>
2018-01-15 00:20:01 +0200

    ADD: Extract features

---
+++ backend/libs/image_calculations/features.py
@@ -0,0 +1,5 @@
+def features(imageDirectory):
+       img=cv2.imread(imageDirectory)
+       histogram=cv2.calcHist([img],[0,1,2],None,  [8,8,8],,256,0,256,0,256])
+       Nhistogram=cv2.normalize(histogram)
+       return Nhistogram.flatten()
