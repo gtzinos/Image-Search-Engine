b019646dd2714b52ade03c9bef2065667e5e725e
George Tzinos <geotzinos@gmail.com>
2018-01-15 01:32:59 +0200

    ADD: Descriptor

--- backend/insert_image.py
+++ backend/insert_image.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from flask import request
+from libs.image_calculations.features import get_features
+from flask import jsonify
+
 app = Flask(__name__)
 app.Debug = True
 
 @app.route("/")
 def hello():
+    features()
     return "<h1>Hello There!</h1>"
+
+@app.route("/upload", methods=['POST'])
+def upload():
+    return 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
