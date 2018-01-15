d0647a6cda41ac48a8400ef5e01f6c0a6389682d
George Tzinos <geotzinos@gmail.com>
2018-01-15 12:11:02 +0200

    FIX: Image feature extraction

--- backend/libs/image_calculations/features.py
+++ backend/libs/image_calculations/features.py
@@ -1,7 +1,19 @@
 import cv2
+import imutils
 
 def get_features(imageDirectory):
-       img=cv2.imread(imageDirectory)
-       histogram=cv2.calcHist([img],[0,1,2],None,  [8,8,8],,256,0,256,0,256])
-       Nhistogram=cv2.normalize(histogram)
-       return Nhistogram.flatten()
+    image=cv2.imread(imageDirectory)
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    histogram = cv2.calcHist([hsv], [0, 1, 2], None, (8,8,8),
+		[0, 180, 0, 256, 0, 256])
+
+    # handle normalizing the histogram if we are using OpenCV 2.4.X
+    if imutils.is_cv2():
+        histogram = cv2.normalize(histogram)
+    # otherwise, perform "in place" normalization in OpenCV 3 (I
+    # personally hate the way this is done
+    else:
+        cv2.normalize(histogram, histogram)
+
+    return histogram.flatten()
