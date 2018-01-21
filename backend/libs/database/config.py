0c77f8b4bef32a0fdc4b0885e14b4a712ac964dd
George Tzinos <geotzinos@gmail.com>
2018-01-22 00:33:18 +0200

    Delete unused

--- backend/libs/database/config.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
-from config import BaseConfig
-
-
-app = Flask(__name__)
-app.config.from_object(BaseConfig)
-db = SQLAlchemy(app)
