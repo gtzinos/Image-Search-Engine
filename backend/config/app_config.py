fac375bc7e3adade422113af14a3c718fe9134e8
George Tzinos <geotzinos@gmail.com>
2018-01-22 01:40:05 +0200

    ADD: Appconfigs

--- backend/config/app_config.py
+++ backend/config/app_config.py
@@ -1,3 +1,13 @@
 class AppConfig:
 
     public_image_url = "http://localhost/"
+
+    database_host = "localhost"
+
+    database_user = "postgres"
+
+    database_password = ""
+
+    database_port = 5432
+
+    database_name = "image_search_engine"
