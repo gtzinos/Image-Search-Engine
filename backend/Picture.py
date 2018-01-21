86cc9e34b135b060833fae264343ae90e4db591c
George Tzinos <geotzinos@gmail.com>
2018-01-21 21:59:31 +0200

    Official postgresql array type

--- backend/Picture.py
+++ backend/Picture.py
@@ -1,13 +1,15 @@
 from app import db
 from sqlalchemy_utils import TSVectorType
+from sqlalchemy.dialects import postgresql
 
 # Create our database model
 class Picture(db.Model):
     __tablename__ = "pictures"
     id = db.Column(db.Integer, primary_key=True)
-    vector = db.Column(TSVectorType, unique=True)
+    vector = db.Column(postgresql.ARRAY(db.Float), unique=True)
     path = db.Column(db.String(120), unique=True)
-    
+    # &&&&&&&&********       Column("data", postgresql.ARRAY(Integer, dimensions=2))
+#http://docs.sqlalchemy.org/en/latest/dialects/postgresql.html#sqlalchemy.dialects.postgresql.ARRAY
     def __init__(self, vector, path):
         self.vector = vector
         self.path = path
