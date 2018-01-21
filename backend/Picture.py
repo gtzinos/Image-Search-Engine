ec418eb1acd749b15ccde6ed0615531ebe083e10
George Tzinos <geotzinos@gmail.com>
2018-01-21 13:36:43 +0200

    FIX: Picture model

--- backend/Picture.py
+++ backend/Picture.py
@@ -1,12 +1,13 @@
 from app import db
+from sqlalchemy_utils import TSVectorType
 
 # Create our database model
 class Picture(db.Model):
     __tablename__ = "pictures"
     id = db.Column(db.Integer, primary_key=True)
-    vector = db.Column(db.ARRAY(db.Float()), unique=True)
+    vector = db.Column(TSVectorType, unique=True)
     path = db.Column(db.String(120), unique=True)
-
+    
     def __init__(self, vector, path):
         self.vector = vector
         self.path = path
