089b4fdf1f87a8d82165026d9f76eb1e4e869b92
George Tzinos <geotzinos@gmail.com>
2018-01-22 01:32:01 +0200

    FIX Picture model

--- backend/Picture.py
+++ backend/Picture.py
@@ -1,15 +1,18 @@
 from app import db
 from sqlalchemy_utils import TSVectorType
+
+from sqlalchemy.dialects.postgresql import TSVECTOR
 from sqlalchemy.dialects import postgresql
 
 # Create our database model
 class Picture(db.Model):
     __tablename__ = "pictures"
     id = db.Column(db.Integer, primary_key=True)
-    vector = db.Column(postgresql.ARRAY(db.Float), unique=True)
+    vector = db.Column(postgresql.ARRAY(db.Float()))
     path = db.Column(db.String(120), unique=True)
+
     # &&&&&&&&********       Column("data", postgresql.ARRAY(Integer, dimensions=2))
-#http://docs.sqlalchemy.org/en/latest/dialects/postgresql.html#sqlalchemy.dialects.postgresql.ARRAY
+    #http://docs.sqlalchemy.org/en/latest/dialects/postgresql.html#sqlalchemy.dialects.postgresql.ARRAY
     def __init__(self, vector, path):
         self.vector = vector
         self.path = path
